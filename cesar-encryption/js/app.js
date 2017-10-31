42863e6ef0af01334f2b768cc2b451b2d49d4752
RosyG <rmga2106@gmail.com>
2017-10-31 17:47:48 -0600

    Actualizando indentación

--- cesar-encryption/js/app.js
+++ cesar-encryption/js/app.js
@@ -1,37 +1,34 @@
 var sentence=prompt('Ingrese una frase');
-
 if(typeof (sentence===''||sentence)!='string'){//Invalido si es campo vacío o no es un string.
  console.log('Invalid sentence');
   }else{
   sentence=sentence.toUpperCase();//Convirtiendo a mayúsculas
-  //console.log(sentence);
   }
-
+//Declaración de variables
 var arraySentence=[];
 var arraySentenceASCII=[];//Guardaré el valor el ASCII con los 33 espacios ya añadidos o disminuidos (según la función: cipher/decipher.
 var arraySentenceEncryp=[];//Letras encriptadas
-var option= decipher(sentence);//¿Encriptado o desencriptado?
-
+var option= cipher(sentence);//¿Encriptado o desencriptado?
+//Función cipher.
 function cipher(sentence){
-arraySentence=Array.from(sentence);
-  for(i=0;i<arraySentence.length;i++){//Obtengo los valores en ASCII de cada letra.
-    var ASCII=arraySentence[i].charCodeAt() ;
-    arraySentenceASCII.push(ASCII+7);//26+7=33 espacios
-   var letterEncryp=String.fromCharCode(ASCII+7);
-   arraySentenceEncryp.push(letterEncryp);
-  }
-return alert(arraySentenceEncryp);
+  arraySentence=Array.from(sentence);
+    for(i=0;i<arraySentence.length;i++){//Obtengo los valores en ASCII de cada letra.
+      var ASCII=arraySentence[i].charCodeAt() ;
+      arraySentenceASCII.push(ASCII+7);//26+7=33 espacios
+      var letterEncryp=String.fromCharCode(ASCII+7);
+      arraySentenceEncryp.push(letterEncryp);
+    }
+  return alert(arraySentenceEncryp);
 }
-
-
+//Función decipher
 function decipher(sentence){
-arraySentence=Array.from(sentence);
-  for(i=0;i<arraySentence.length;i++){//Obtengo los valores en ASCII de cada letra.
-    var ASCII=arraySentence[i].charCodeAt() ;
-    arraySentenceASCII.push(ASCII-7);//Regreso 7 para volver a la misma posición
-    var letterEncryp=String.fromCharCode(ASCII-7);
-    arraySentenceEncryp.push(letterEncryp);
-  }
+  arraySentence=Array.from(sentence);
+    for(i=0;i<arraySentence.length;i++){//Obtengo los valores en ASCII de cada letra.
+      var ASCII=arraySentence[i].charCodeAt() ;
+      arraySentenceASCII.push(ASCII-7);//Regreso 7 para volver a la misma posición
+      var letterEncryp=String.fromCharCode(ASCII-7);
+      arraySentenceEncryp.push(letterEncryp);
+    }
   //arraySentenceEncryp.toString();
-return alert(arraySentenceEncryp);
+  return alert(arraySentenceEncryp);
 }
--- valid-credit-card/js/app.js
+++ valid-credit-card/js/app.js
@@ -3,40 +3,38 @@ var arrayString=[];
 var arrayNumber=[];
 var arrayAux=[];
 var arrayMultiply=[];
-//number=parseInt(number);
+//Comprobando que se ingrese solo números
 if(number===' '){
   number=prompt('No valido, introduce sólo números.');
-}else{
-  arrayString= Array.from(number);
-  for (i=0;i<arrayString.length;i++){//Obteniendo un arreglo de los números introducidos como string.
-  arrayNumber.push(parseInt(arrayString[i]));
-    if(i%2===0){//Armando un arreglo con 1 y 2 como elementos.
+  }else{
+    arrayString= Array.from(number);
+    for (i=0;i<arrayString.length;i++){//Obteniendo un arreglo de los números introducidos como string.
+      arrayNumber.push(parseInt(arrayString[i]));
+      if(i%2===0){//Armando un arreglo con 1 y 2 como elementos.
       arrayAux.push(1);
-    }else{
-      arrayAux.push(2);
+      }else{
+        arrayAux.push(2);
+        }
     }
-  }
   //console.log(arrayAux);
-}
+  }
 
 isValidCard(arrayNumber);//Mandando a llamar a la función para validar tarjeta
 
 function isValidCard(array){
-array.reverse();//Invierto los elementos del array
-var ResOfSum=0;//Variable que guardara la suma de los elementos del arrayMultiply
-for(j=0;j<arrayNumber.length;j++){
-  var multiply=arrayNumber[j]*arrayAux[j]
-  if(multiply>9){
-    multiply=multiply-9;//Restar 9 en caso de que al multiplicar por 2, su valor es mayor a 9.
-   }
-arrayMultiply.push(multiply);//Arreglo con los elementos ya alterados
-  ResOfSum= arrayMultiply[j]+ResOfSum;//Se guarda la suma de cada elemento de arrayMultiply.
-  }
-
-  if(ResOfSum%10===0){
-  return alert('Valid card number');
-    }else{
-    return alert ('Sorry, invalid card number');
+  array.reverse();//Invierto los elementos del array
+  var ResOfSum=0;//Variable que guardara la suma de los elementos del arrayMultiply
+  for(j=0;j<arrayNumber.length;j++){
+    var multiply=arrayNumber[j]*arrayAux[j]
+    if(multiply>9){
+      multiply=multiply-9;//Restar 9 en caso de que al multiplicar por 2, su valor es mayor a 9.
     }
-//return console.log(ResOfSum);
+    arrayMultiply.push(multiply);//Arreglo con los elementos ya alterados
+    ResOfSum= arrayMultiply[j]+ResOfSum;//Se guarda la suma de cada elemento de arrayMultiply.
   }
+  if(ResOfSum%10===0){
+    return alert('Valid card number');
+    }else{
+      return alert ('Sorry, invalid card number');
+    }
+}
